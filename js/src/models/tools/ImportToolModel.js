2b54c5e7535962d3d5287b8167896e7f1b9ab5f7
pixelmaid <jacobsj@media.mit.edu>
2014-07-07 08:24:15 -0700

    adding import

---
+++ js/src/models/tools/ImportToolModel.js
@@ -0,0 +1,39 @@
+/*ImportToolModel.js
+ * imports in shapes from svg files to canvas
+ */
+
+define([
+	'underscore',
+	'backbone',
+	'models/tools/BaseToolModel',
+	'models/data/PathNode',
+	'models/PaperManager'
+
+], function(_, Backbone, BaseToolModel, PathNode, PaperManager) {
+
+	var ImportToolModel = BaseToolModel.extend({
+
+	initialize: function(){
+
+	},
+
+	import: function( filename){
+		var paper = PaperManager.getPaperInstance();
+			var files = filename
+			for (var i = 0; i < files.length; i++) {
+				var file = files[i];
+				if (file.type.match('svg')) {
+					paper.project.importSVG(file, function(item) {
+						console.log(item);
+					});
+				}
+			}
+		});
+	}
+
+
+	});
+
+	return SelectToolModel;
+
+});
