3e8e2eb17a6dd5189f6a635152ebf2681b32ef2f
jejacobs <jejacobs@adobe.com>
2014-09-27 14:53:07 -0400

    removed reference to minicolors in PropertyView

--- js/src/views/drawing/PropertyView.js
+++ js/src/views/drawing/PropertyView.js
@@ -10,8 +10,6 @@ define([
   'tinycolor',
   'pickacolor',
   'models/PaperManager',
-
-  'minicolors',
   'slider'
 
 
@@ -54,22 +52,7 @@ define([
         });
       });
 
-      /*$('.demo').each( function() {   
- $(this).minicolors({
-  control: $(this).attr('data-control') || 'hue',
-  defaultValue: $(this).attr('data-defaultValue') || '',
-  inline: $(this).attr('data-inline') === 'true',
-  letterCase: $(this).attr('data-letterCase') || 'lowercase',
-  opacity: $(this).attr('data-opacity'),
-  position: $(this).attr('data-position') || 'bottom left',
-  change: function(hex, opacity) {
-    if( !hex ) {return;}
-    if( opacity ){ hex += ', ' + opacity;}
-    $(this).trigger('color-change');
-  },
-  theme: 'bootstrap'
-});
-});*/
+
     },
 
     events: {
