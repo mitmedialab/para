b62c0920a1d0a8e177bff980b1a9bf2b00cfc071
Jennifer Jacobs <jacobsj@media.mit.edu>
2014-12-15 14:46:22 -0800

    implemented rectangle and ellipse tools

--- js/src/models/tools/PolyToolModel.js
+++ js/src/models/tools/PolyToolModel.js
@@ -12,23 +12,43 @@ define([
   var nameVal = 0;
   var sideNum = 6;
   var rotationAmt = 0;
+  var startPosition = null;
   var scaleAmt = 0;
   var polyPath = null;
   var drag = false;
-  var literal  = null;
+  var literal = null;
+  var lastDeltaX;
+  var lastDeltaY;
   var PolyToolModel = BaseToolModel.extend({
-    defaults: _.extend({}, BaseToolModel.prototype.defaults, {}),
+    defaults: _.extend({}, BaseToolModel.prototype.defaults, {
+      mode: 'poly'
+    }),
 
     initialize: function() {
       BaseToolModel.prototype.initialize.apply(this, arguments);
 
     },
 
-    /*mousedown event- checks to see if current path has been initialized-
-     *if it has not, create a new one and trigger a shapeAdded event
+    /*mousedown event
      */
     mouseDown: function(event) {
-      polyPath = new paper.Path.RegularPolygon(event.point, sideNum, 1);
+      startPosition = event.point;
+      switch (this.get('mode')) {
+        case 'poly':
+          polyPath = new paper.Path.RegularPolygon(event.point, sideNum, 1);
+          break;
+        default:
+          var rectangle = new paper.Rectangle(event.point, new paper.Size(1, 1));
+          switch (this.get('mode')) {
+            case 'ellipse':
+              polyPath = new paper.Path.Ellipse(rectangle);
+              break;
+            case 'rect':
+              polyPath = new paper.Path.Rectangle(rectangle);
+              break;
+          }
+          break;
+      }
       polyPath.selected = true;
       polyPath.strokeWidth = this.get('stroke_width');
       polyPath.strokeColor = this.get('stroke_color');
@@ -45,8 +65,6 @@ define([
       rotationAmt = 0;
     },
 
-
-
     //mouse up event
     mouseUp: function(event) {
       if (polyPath) {
@@ -55,13 +73,12 @@ define([
           matrix.reset();
           matrix.translate(polyPath.bounds.center.x, polyPath.bounds.center.y);
           matrix.rotate(rotationAmt);
-          
-          literal = polyPath;
-         
-          this.trigger('geometryAdded',literal);
 
-        }
-        else{
+          literal = polyPath;
+
+          this.trigger('geometryAdded', literal);
+
+        } else {
           polyPath.remove();
         }
         polyPath = null;
@@ -73,6 +90,17 @@ define([
 
     //mouse drag event
     mouseDrag: function(event) {
+      switch (this.get('mode')) {
+        case 'poly':
+          this.polyMouseDrag(event);
+          break;
+        default:
+          this.regularMouseDrag(event);
+          break;
+      }
+    },
+
+    polyMouseDrag: function(event) {
 
       if (polyPath) {
         drag = true;
@@ -85,6 +113,41 @@ define([
         rotationAmt += rotate;
         polyPath.scale(scale);
         polyPath.rotate(rotate);
+
+      }
+
+    },
+    //mouse drag event
+    regularMouseDrag: function(event) {
+      if (polyPath) {
+        drag = true;
+
+        var deltaX = event.point.x - startPosition.x;
+        var deltaY = event.point.y - startPosition.y;
+
+        polyPath.remove();
+        var rectangle = new paper.Rectangle( startPosition, new paper.Size(deltaX, deltaY));
+        if(this.get('mode')==='rect'){    
+          polyPath = new paper.Path.Rectangle(rectangle);
+        }
+        else{
+          polyPath = new paper.Path.Ellipse(rectangle);
+        }
+        
+        polyPath.selected = true;
+        polyPath.strokeWidth = this.get('stroke_width');
+        polyPath.strokeColor = this.get('stroke_color');
+        polyPath.fillColor = this.get('fill_color');
+
+      if (this.get('fill_color') === -1) {
+        polyPath.style.fillColor = null;
+
+      }
+      if (this.get('stroke_color') === -1) {
+        polyPath.style.strokeColor = null;
+
+      }    
+        console.log("path data", polyPath.bounds,deltaX,deltaY);
 
       }
 
--- js/src/views/drawing/ToolView.js
+++ js/src/views/drawing/ToolView.js
@@ -1,5 +1,5 @@
 /*ToolView.js
-* master view for all drawing tools */
+ * master view for all drawing tools */
 
 define([
   'jquery',
@@ -7,110 +7,167 @@ define([
   'backbone',
   'utils/analytics'
 
-], function($, _, Backbone, analytics){
+], function($, _, Backbone, analytics) {
 
   var lastSelected;
   var eventType = 'toolChange';
   var ToolView = Backbone.View.extend({
     //
 
-    initialize: function(){
+    initialize: function() {
       this.polyToolClick();
     },
 
-    render: function(){
- 
+    render: function() {
+
     },
 
-     events: {
-    'click #selectTool': 'selectToolClick',
-        'click #dselectTool': 'dSelectToolClick',
+    events: {
+      'click #selectTool': 'selectToolClick',
+      'click #dselectTool': 'dSelectToolClick',
+      'click #penTool': 'penToolClick',
+      'click #polyTool': 'polyToolClick',
+      'click #rectangleTool': 'rectToolClick',
+      'click #ellipseTool': 'ellipseToolClick',
+      'click #rotateTool': 'rotateToolClick',
+      'click #followPathTool': 'followPathToolClick',
+      'click #undoTool': 'undoToolClick',
+      'click #redoTool': 'redoToolClick'
 
-    'click #penTool': 'penToolClick',
-    'click #polyTool': 'polyToolClick',
-    'click #rotateTool': 'rotateToolClick',
-    'click #followPathTool': 'followPathToolClick',
-    'click #undoTool': 'undoToolClick',
-    'click #redoTool': 'redoToolClick'
+    },
 
-  	},
+    undoToolClick: function() {
+      this.model.undo();
+      analytics.log(eventType, {
+        type: eventType,
+        id: 'undo',
+        action: 'undo'
+      });
+    },
 
-      undoToolClick: function(){
-        this.model.undo();
-        analytics.log(eventType,{type:eventType,id:'undo',action:'undo'});
-      },
+    redoToolClick: function() {
+      this.model.redo();
+      analytics.log(eventType, {
+        type: eventType,
+        id: 'redo',
+        action: 'redo'
+      });
 
-      redoToolClick: function(){
-        this.model.redo();
-        analytics.log(eventType,{type:eventType,id:'redo',action:'redo'});
+    },
 
-      },
+    selectToolClick: function() {
+      this.clearActive();
+      $('#selectTool').addClass('active');
+      lastSelected = $('#selectTool');
+      this.model.setState('selectTool', 'select');
+      analytics.log(eventType, {
+        type: eventType,
+        id: 'selectTool',
+        action: 'toolSelected'
+      });
 
-  	 selectToolClick: function(){
-  		this.clearActive();
-  		$('#selectTool').addClass('active');
-  		lastSelected = $('#selectTool');
-      this.model.setState('selectTool','select');
-      analytics.log(eventType,{type:eventType,id:'selectTool',action:'toolSelected'});
 
+    },
 
-  	},
-
-    dSelectToolClick: function(){
+    dSelectToolClick: function() {
       this.clearActive();
       $('#dselectTool').addClass('active');
       lastSelected = $('#dselectTool');
-      this.model.setState('selectTool','dselect');
-      analytics.log(eventType,{type:eventType,id:'dselectTool',action:'toolSelected'});
+      this.model.setState('selectTool', 'dselect');
+      analytics.log(eventType, {
+        type: eventType,
+        id: 'dselectTool',
+        action: 'toolSelected'
+      });
     },
 
-     followPathToolClick: function(){
+    followPathToolClick: function() {
       this.clearActive();
       $('#followPathTool').addClass('active');
       lastSelected = $('#followPathTool');
       this.model.setState('followPathTool');
-      analytics.log(eventType,{type:eventType,id:'followPathTool',action:'toolSelected'});
+      analytics.log(eventType, {
+        type: eventType,
+        id: 'followPathTool',
+        action: 'toolSelected'
+      });
     },
 
-  	penToolClick: function(){
-  		this.clearActive();
-  		$('#penTool').addClass('active');
-  		lastSelected = $('#penTool');
+    penToolClick: function() {
+      this.clearActive();
+      $('#penTool').addClass('active');
+      lastSelected = $('#penTool');
       this.model.setState('penTool');
-      analytics.log(eventType,{type:eventType,id:'penTool',action:'toolSelected'});
+      analytics.log(eventType, {
+        type: eventType,
+        id: 'penTool',
+        action: 'toolSelected'
+      });
 
 
-  	},
+    },
 
-  	polyToolClick: function(){
-  		this.clearActive();
-  		$('#polyTool').addClass('active');
-  		lastSelected = $('#polyTool');
-      this.model.setState('polyTool');
-      analytics.log(eventType,{type:eventType,id:'polyTool',action:'toolSelected'});
+    rectToolClick: function() {
+      this.clearActive();
+      $('#rectangleTool').addClass('active');
+      lastSelected = $('#rectangleTool');
+      this.model.setState('polyTool', 'rect');
+      analytics.log(eventType, {
+        type: eventType,
+        id: 'rectangleTool',
+        action: 'toolSelected'
+      });
 
 
-  	},
+    },
 
-    rotateToolClick: function(){
+    ellipseToolClick: function() {
+      this.clearActive();
+      $('#ellipseTool').addClass('active');
+      lastSelected = $('#ellipseTool');
+      this.model.setState('polyTool', 'ellipse');
+      analytics.log(eventType, {
+        type: eventType,
+        id: 'ellipseTool',
+        action: 'toolSelected'
+      });
+
+
+    },
+
+    polyToolClick: function() {
+      this.clearActive();
+      $('#polyTool').addClass('active');
+      lastSelected = $('#polyTool');
+      this.model.setState('polyTool', 'poly');
+      analytics.log(eventType, {
+        type: eventType,
+        id: 'polyTool',
+        action: 'toolSelected'
+      });
+    },
+
+    rotateToolClick: function() {
       this.clearActive();
       $('#rotateTool').addClass('active');
       lastSelected = $('#rotateTool');
-      this.model.setState('selectTool','rotate');
+      this.model.setState('selectTool', 'rotate');
       this.model.selectTool.
-      analytics.log(eventType,{type:eventType,id:'rotateTool',action:'toolSelected'});
-
-
+      analytics.log(eventType, {
+        type: eventType,
+        id: 'rotateTool',
+        action: 'toolSelected'
+      });
     },
 
-  	clearActive: function(){
-  		if(lastSelected){
-  			lastSelected.removeClass('active');
-  		}
-  	}
+    clearActive: function() {
+      if (lastSelected) {
+        lastSelected.removeClass('active');
+      }
+    }
 
   });
 
   return ToolView;
-  
+
 });
